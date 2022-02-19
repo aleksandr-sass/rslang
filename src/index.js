7f4209ab67ca9ecc682587b5f2de82ac85be9c91
aleksandr-sass <a.sass@tut.by>
2022-02-19 19:59:33 +0300

    feat: add view of infomation about a single word

---
+++ src/index.js
@@ -0,0 +1,26 @@
+let wordInfo = document.querySelector("#word-info");
+let baseURL = new URL("https://rslang-aleksandr-sass.herokuapp.com/");
+let wordURL = baseURL;
+wordURL.pathname="/words/5e9f5ee35eb9e72bc21af4a2";
+
+
+fetch(wordURL.href)
+  .then(response => response.json())
+  .then(data => {
+    let imageURL = baseURL;
+    imageURL.pathname = data.image;
+    let word = data.word;
+    let transcription = data.transcription;
+    let wordTranslate = data.wordTranslate;
+    let textMeaning = data.textMeaning;
+    let textMeaningTranslate = data.textMeaningTranslate;
+    let textExample = data.textExample;
+    let textExampleTranslate = data.textExampleTranslate;
+    wordInfo.innerHTML = `<img src="${imageURL.href}" alt="${word}">
+                          <p>${word} ${transcription}</p>
+                          <p>${wordTranslate}</p>
+                          <p>${textMeaning}</p>
+                          <p>${textMeaningTranslate}</p>
+                          <p>${textExample}</p>
+                          <p>${textExampleTranslate}</p>`;
+  });
