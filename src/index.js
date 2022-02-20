25517f7b67a49ef644a26ffc2a9afe8568512295
aleksandr-sass <a.sass@tut.by>
2022-02-20 14:31:47 +0300

    feat: add view for page of 20 words

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-let wordInfo = document.querySelector("#word-info");
+let pageInfo = document.querySelector("#page-info");
 let site = "https://rslang-aleksandr-sass.herokuapp.com/";
-let wordData = {};
-let wordURL = new URL("words/5e9f5ee35eb9e72bc21af4a2", site);
+let pageData = [];
+let pageURL = new URL("words?group=0&page=0", site);
 
-fetch(wordURL)
+fetch(pageURL)
   .then(response => response.json())
   .then(data => {
-    wordData = data;
-    wordInfo.innerHTML = getCardHTML(data);
-    let soundButton = wordInfo.querySelector("img[name='soundButton']");
-    soundButton.addEventListener("click", listenAudio);
+    pageData = data;
+    pageInfo.innerHTML = getPageHTML(data);
+    initializeAudio(data);
   });
+
+function getPageHTML(data) {
+  let content = '';
+  for (let i = 0; i < data.length; i += 1) {
+    let wordContent = getCardHTML(data[i]);
+    content += `<div id="word-${i}">${wordContent}</div>`;
+  };
+  return content;
+}
 
 function getCardHTML(data) {
   return `<img src="${(new URL(data.image, site)).href}" alt="${data.word}" class="round">
@@ -23,18 +31,27 @@ function getCardHTML(data) {
     <p>${data.textExampleTranslate}</p>`;
 }
 
-function listenAudio() {
-  let soundButton = wordInfo.querySelector("img[name='soundButton']");
-  soundButton.src = "../img/audioOn.png";
-  let soundEffect = prepareAudio(wordData);
-  playAudio(soundEffect);
+function initializeAudio(data) {
+  for (let i = 0; i < data.length; i += 1) {
+    let soundButton = pageInfo.querySelector(`#word-${i} img[name="soundButton"]`);
+    soundButton.addEventListener("click", listenAudio);
+  }
 }
 
-function prepareAudio(wordData) {
+function listenAudio() {
+  let soundButton = this;
+  let soundId = getId(soundButton);
+  soundButton.src = "../img/audioOn.png";
+  let soundEffect = prepareAudio(pageData[soundId]);
+  playAudio(soundEffect);
+  soundEffect[2].addEventListener("ended", () => soundButton.src = "../img/audio.png");
+}
+
+function prepareAudio(data) {
   return [
-    new Audio(new URL(wordData.audio, site)),
-    new Audio(new URL(wordData.audioMeaning, site)),
-    new Audio(new URL(wordData.audioExample, site))
+    new Audio(new URL(data.audio, site)),
+    new Audio(new URL(data.audioMeaning, site)),
+    new Audio(new URL(data.audioExample, site))
   ];
 }
 
@@ -42,6 +59,12 @@ function playAudio(soundEffect) {
   soundEffect[0].play();
   soundEffect[0].addEventListener("ended", () => soundEffect[1].play());
   soundEffect[1].addEventListener("ended", () => soundEffect[2].play());
-  soundEffect[2].addEventListener("ended", () => soundButton.src = "../img/audio.png");
-  return 0;
+}
+
+function getId(element) {
+  return element
+    .parentNode
+    .parentNode
+    .id
+    .split('-')[1];
 }
