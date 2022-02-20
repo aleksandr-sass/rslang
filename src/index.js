cfc2d4b72d82e15d64296dbd0f6e41905217e633
aleksandr-sass <a.sass@tut.by>
2022-02-20 11:23:30 +0300

    refactor: rewrite 'index.js'

--- src/index.js
+++ src/index.js
@@ -1,54 +1,47 @@
 let wordInfo = document.querySelector("#word-info");
 let site = "https://rslang-aleksandr-sass.herokuapp.com/";
-let baseURL = new URL(site);
-let wordURL = new URL(site);
 let wordData = {};
-wordURL.pathname="/words/5e9f5ee35eb9e72bc21af4a2";
+let wordURL = new URL("words/5e9f5ee35eb9e72bc21af4a2", site);
 
-
-fetch(wordURL.href)
+fetch(wordURL)
   .then(response => response.json())
   .then(data => {
     wordData = data;
-    let imageURL = baseURL;
-    imageURL.pathname = data.image;
-    let word = data.word;
-    let transcription = data.transcription;
-    let wordTranslate = data.wordTranslate;
-    let textMeaning = data.textMeaning;
-    let textMeaningTranslate = data.textMeaningTranslate;
-    let textExample = data.textExample;
-    let textExampleTranslate = data.textExampleTranslate;
-    wordInfo.innerHTML = `<img src="${imageURL.href}" alt="${word}" class="round">
-                          <p>${word} ${transcription}</p>
-                          <p><img src="../img/audio.png" alt="audio" id="soundButton"></p>
-                          <p>${wordTranslate}</p>
-                          <p>${textMeaning}</p>
-                          <p>${textMeaningTranslate}</p>
-                          <p>${textExample}</p>
-                          <p>${textExampleTranslate}</p>`;
-
-    let soundButton = wordInfo.querySelector("#soundButton");
+    wordInfo.innerHTML = getCardHTML(data);
+    let soundButton = wordInfo.querySelector("img[name='soundButton']");
     soundButton.addEventListener("click", listenAudio);
   });
 
+function getCardHTML(data) {
+  return `<img src="${(new URL(data.image, site)).href}" alt="${data.word}" class="round">
+    <p>${data.word} ${data.transcription}</p>
+    <p><img src="../img/audio.png" alt="audio" name="soundButton"></p>
+    <p>${data.wordTranslate}</p>
+    <p>${data.textMeaning}</p>
+    <p>${data.textMeaningTranslate}</p>
+    <p>${data.textExample}</p>
+    <p>${data.textExampleTranslate}</p>`;
+}
 
 function listenAudio() {
-  let soundButton = wordInfo.querySelector("#soundButton");
+  let soundButton = wordInfo.querySelector("img[name='soundButton']");
   soundButton.src = "../img/audioOn.png";
-  
-  let soundURL = new URL(site);
+  let soundEffect = prepareAudio(wordData);
+  playAudio(soundEffect);
+}
 
-  soundURL.pathname = wordData.audio;
-  let soundEffect1 = new Audio(soundURL.href);
+function prepareAudio(wordData) {
+  return [
+    new Audio(new URL(wordData.audio, site)),
+    new Audio(new URL(wordData.audioMeaning, site)),
+    new Audio(new URL(wordData.audioExample, site))
+  ];
+}
 
-  soundURL.pathname = wordData.audioMeaning;
-  let soundEffect2 = new Audio(soundURL.href);
-
-  soundURL.pathname = wordData.audioExample;
-  let soundEffect3 = new Audio(soundURL.href);
-  soundEffect1.play();
-  soundEffect1.addEventListener("ended", () => soundEffect2.play());
-  soundEffect2.addEventListener("ended", () => soundEffect3.play());
-  soundEffect3.addEventListener("ended", () => soundButton.src = "../img/audio.png");
+function playAudio(soundEffect) {
+  soundEffect[0].play();
+  soundEffect[0].addEventListener("ended", () => soundEffect[1].play());
+  soundEffect[1].addEventListener("ended", () => soundEffect[2].play());
+  soundEffect[2].addEventListener("ended", () => soundButton.src = "../img/audio.png");
+  return 0;
 }
